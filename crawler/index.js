984b4e969084c72ed2310771f6b7707decae6d71
Spencer Zhang <sepmein@gmail.com>
2013-04-28 10:54:54 +0800

    分离测试代码

--- crawler/index.js
+++ crawler/index.js
@@ -2,7 +2,9 @@ var request = require('request');
 var async = require('async');
 var githubToken = require('.././config').githubToken;
 
-var getRepositoriesSeries = function getRepositoriesSeries(arg, callback) {
+var Crawler = {};
+
+var makeRequest = function makeRequest(arg, callback) {
     var option = (function (argument) {
         var querySince;
         if (argument > 0) {
@@ -15,7 +17,7 @@ var getRepositoriesSeries = function getRepositoriesSeries(arg, callback) {
             qs: {
                 since: argument,
                 'client_id': githubToken.client_id,
-                'client_secret':githubToken.client_secret
+                'client_secret': githubToken.client_secret
             },
             headers: {
                 "User-Agent": "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.64 Safari/537.31"
@@ -34,25 +36,27 @@ var getRepositoriesSeries = function getRepositoriesSeries(arg, callback) {
 
 
 //profiling
-(function () {
+Crawler.getGithubRepositories = function () {
 
-    var   startingTime = new Date();
+    var startingTime = new Date();
     var counter = 0;
     async.waterfall(
         [function (callback) {
-            getRepositoriesSeries(null, callback);
+            makeRequest(null, callback);
         }, function (arg, callback) {
-            getRepositoriesSeries(arg, callback);
+            makeRequest(arg, callback);
         }, function (arg, callback) {
-            getRepositoriesSeries(arg, callback);
+            makeRequest(arg, callback);
         }, function (arg, callback) {
-            getRepositoriesSeries(arg, callback);
+            makeRequest(arg, callback);
         }, function (arg, callback) {
-            getRepositoriesSeries(arg, callback);
+            makeRequest(arg, callback);
         }
         ], function (err, result) {
             var endingTime = new Date();
             var processingTime = (endingTime - startingTime) / 1000;
             console.log('处理总共用时 : ' + processingTime + '秒');
         });
-})();
+};
+
+module.exports = Crawler;
