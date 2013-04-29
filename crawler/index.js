42294297008ff307b59464919ab56740d9a97adf
Spencer <sepmein@gmail.com>
2013-04-29 19:50:04 +0800

    some modify

--- crawler/index.js
+++ crawler/index.js
@@ -2,57 +2,82 @@ var request = require('request');
 var async = require('async');
 var githubToken = require('.././config').githubToken;
 
-var getRepositoriesSeries = function getRepositoriesSeries(arg, callback) {
-    var option = (function (argument) {
-        var querySince;
-        if (argument > 0) {
-            querySince = argument;
-        } else {
-            querySince = 0;
-        }
-        return {
-            url: "https://api.github.com/repositories",
-            qs: {
-                since: argument,
-                'client_id': githubToken.client_id,
-                'client_secret':githubToken.client_secret
-            },
-            headers: {
-                "User-Agent": "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.64 Safari/537.31"
-            }
-        }
-    })(arg);
+var Crawler = Object.create(null);
 
-    request(option, function (error, response, body) {
-        var results = JSON.parse(body);
-        var lastId = results[results.length - 1].id;
-        //console.dir(response);
-        console.log(lastId);
-        callback(null, lastId);
-    });
+Crawler.makeRequest = function makeRequest(arg, callback) {
+	var option = (function (argument) {
+		var querySince;
+		if (argument > 0) {
+			querySince = argument;
+		} else {
+			querySince = 0;
+		}
+		return {
+			url: "https://api.github.com/repositories",
+			qs: {
+				since: argument,
+				'client_id': githubToken.client_id,
+				'client_secret': githubToken.client_secret
+			},
+			headers: {
+				"User-Agent": "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.64 Safari/537.31"
+			}
+		}
+	})(arg);
+
+	request(option, function (error, response, body) {
+		var results = JSON.parse(body);
+		var lastId = results[results.length - 1].id;
+		//console.dir(response);
+		console.log(lastId);
+		callback(null, lastId);
+	});
 };
 
 
 //profiling
-(function () {
+Crawler.getGithubRepositories = function () {
 
-    var   startingTime = new Date();
-    var counter = 0;
-    async.waterfall(
-        [function (callback) {
-            getRepositoriesSeries(null, callback);
-        }, function (arg, callback) {
-            getRepositoriesSeries(arg, callback);
-        }, function (arg, callback) {
-            getRepositoriesSeries(arg, callback);
-        }, function (arg, callback) {
-            getRepositoriesSeries(arg, callback);
-        }, function (arg, callback) {
-            getRepositoriesSeries(arg, callback);
-        }
-        ], function (err, result) {
-            var endingTime = new Date();
-            var processingTime = (endingTime - startingTime) / 1000;
-            console.log('处理总共用时 : ' + processingTime + '秒');
-        });
-})();
+	var startingTime = new Date();
+	var counter = 0;
+	async.waterfall(
+		[function (callback) {
+			makeRequest(null, callback);
+		}, function (arg, callback) {
+			makeRequest(arg, callback);
+		}, function (arg, callback) {
+			makeRequest(arg, callback);
+		}, function (arg, callback) {
+			makeRequest(arg, callback);
+		}, function (arg, callback) {
+			makeRequest(arg, callback);
+		}
+		], function (err, result) {
+			var endingTime = new Date();
+			var processingTime = (endingTime - startingTime) / 1000;
+			console.log('处理总共用时 : ' + processingTime + '秒');
+		});
+};
+
+module.exports = Crawler;
+
+//master worker mode
+//master 分配任务
+//worker 完成任务，将完成信息反馈至master处，将数据存入数据库
+//worker 可扩展
+//worker 无状态
+var crawler = {
+	master: {
+		assignTask: '',
+        loadBalance: '',
+		createWorker: '',
+		destoryWorker: ''
+	},
+	worker: {
+		id:'',
+		onAssign: '',
+	    onFinish: '',
+		saveToDb: ''
+
+	}
+}
