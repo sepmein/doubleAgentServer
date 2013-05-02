d173d076f07faa6a30add8cb9633021926684668
Spencer <sepmein@gmail.com>
2013-05-02 10:34:33 +0800

    seperate job level

--- experiment.js
+++ experiment.js
@@ -3,8 +3,6 @@
 var util = require('./util');
 // create a task, run that task, get a result from that task, create a task based on that
 
-//controller.on('end',function(){});
-
 //when task finished deliver the data to the controller and db
 //send a message to the controller
 //controller delete task
@@ -13,22 +11,30 @@ var util = require('./util');
 var EventEmitter = require('events').EventEmitter;
 var master = Object.create(EventEmitter.prototype);
 //Object.getPrototypeOf(master);
-
 master.on('taskFinished', function(seal, produce) {
-	slaves.bornABabySlave(since);
+	slaves.bornABabySlave(since, this.job);
 	slaves.killTheElderSlave(seal);
 	//	console.log('taskFinished and since is :' + since);
 });
 master.on('taskUnfinished',function(seal){
 
 });
+
+//data level of jobs
+var job = function(cb){
+	//just for testment
+	cb('job done');
+};
+
+
 //prototype of Slave
 //seal is the only id of the slave
 //印章是奴隶的唯一标示
-var Slave = function Slave(since) {
+var Slave = function Slave(since, job) {
 	this.seal = util.generateRandom(20);
 	this.since = since || 0;
 	this.produce = null;
+	this.job = job || null;
 };
 //when task is finished send a message to master 
 //当任务完成时，向主人发送一条消息
@@ -40,6 +46,14 @@ Slave.prototype.sendMessageToMaster = function() {
 	}
 
 };
+Slave.prototype.workWork = function(job) {
+	var self = this;
+	function whenDone(produce){
+		self.produce = produce;
+		self.sendMessageToMaster();
+	}
+	job(whenDone);
+};
 
 //slaves
 //Data level about slave
@@ -49,8 +63,8 @@ slaves.list = {};
 //born a baby slave
 //@type: function
 //@param: Since (type:num) - github api params get repositories from 'since'
-slaves.bornABabySlave = function(since) {
-	var newBornSlave = new Slave(since);
+slaves.bornABabySlave = function(since, job) {
+	var newBornSlave = new Slave(since, job);
 	this.list[newBornSlave.seal] = newBornSlave;
 	console.dir(this.list);
 	return newBornSlave.seal;
@@ -73,6 +87,7 @@ assert.notEqual(slave1.seal, null);
 //重新新建的slave对象应与刚才建立的对象不同
 var slave2 = new Slave();
 assert.notEqual(slave1.seal, slaves.seal);
+
 var babySeal = slaves.bornABabySlave(100);
 var killed = slaves.killTheElderSlave(babySeal);
 assert.strictEqual(killed, true);
