566fde9cc85ef3ae18136c6798589e8252409b5b
Spencer Zhang <sepmein@gmail.com>
2013-06-09 16:32:29 +0800

    working on github search

--- crawler/search.js
+++ crawler/search.js
@@ -7,11 +7,12 @@ var Infiniteloop = require('Infinite-loop');
 var makeRequest = require('.././crawler').makeRequest;
 var bisection = require('.././util').bisection;
 var targetGenerator = require('./targetGenerator');
+var github = require('.././github');
 
-var dosomething = function(ruler, cb) {
+var doSomething = function (ruler, cb) {
 
     var FIRSTGUESE = 1143,
-        //todo modify it
+    //todo modify it
         FIRSTGAP = 100,
         INCREMENT = 20,
         currentGuess = FIRSTGUESE,
@@ -30,30 +31,95 @@ var dosomething = function(ruler, cb) {
     if (ruler.length === 0) {
         //init
         //db.check ruler.length later
-        function firstOperation() {
+        (function firstOperation() {
             github.search.staredRepos.checkLength({
-                urlAppend: {
-                    type: '>',
+                urlAppend: 
+                {
+                    type: '>', 
                     args: [currentGuess]
                 }
-            }, function(err, result) {
+            }, function (err, result) {
+                    if (err) throw err;
+                    if (downTimes * upTimes > 0) {
+                        //如果预测值的上下数据都有了
+                        //现在才是两分法啊！！！尼玛，算法太复杂了
+                        bisection(
+                            closestUp, closestDown, checkBiggerThan, function (result) {
+                                //craw the result
+                                crawlStaredRepos(result);
+                                //deliver the result
+                                cb(result);
+                            });
+                    } else {
+                        switch (result) {
+                            case 0:
+                                //恰当，可以不用做两分法测试了,应该直接使用该值抓取数据
+                                //由于是首次抓取所以情况特殊，searchTerm应使用特别语句生成
+                                crawlStaredRepos(currentGuess);
+                                cb(currentGuess);
+                                break;
+                            case 1:
+                                //返回结果数量太多， 减少猜想值
+                                //继续
+                                if (lastOperation.operation === 'down') {
+                                    lastOperation.optimes++;
+                                    currentGuess -= INCREMENT * lastOperation.optimes;
+                                } else {
+                                    //rest optimes
+                                    lastOperation.optimes = 0;
+                                    currentGuess -= INCREMENT;
+                                }
+                                lastOperation.operation = 'down';
+                                //zai shang mian de ci shu
+                                upTimes++;
+                                closestUp = currentGuess;
+                                firstOperation();
+                                break;
+                            case -1:
+                                //返回结果数量太少， 增加猜想值
+                                //go on
+                                if (lastOperation.operation === 'up') {
+                                    lastOperation.optimes++;
+                                    currentGuess += INCREMENT * lastOperation.optimes;
+                                } else {
+                                    //rest optimes
+                                    lastOperation.optimes = 0;
+                                    currentGuess += INCREMENT;
+                                }
+                                lastOperation.operation = 'up';
+                                downTimes++;
+                                closestDown = currentGuess;
+                                firstOperation();
+                                break;
+                        }
+                    }
+                });
+        })();
+    } else {
+        (function nextStep() { //first Operation has been done
+            //use the latest info in the array
+            var gap = (rulerLength > 1) ? (lastRuler - ruler[rulerLength - 2]) : FIRSTGAP;
+            //guess the current gap using the latest gap
+            var guess = lastRuler - gap / 0.9;
+            github.search.staredRepos.checkLength({
+                type: '..',
+                args: [guess, lastRuler]
+            }, function (err, result) {
                 if (err) throw err;
                 if (downTimes * upTimes > 0) {
-                    //如果预测值的上下数据都有了
-                    //现在才是两分法啊！！！尼玛，算法太复杂了
                     bisection(
-                        closestUp, closestDown, checkBiggerThan, function(result) {
-                        //craw the result
-                        stadardPageCrawler(result);
-                        //diliver the result
-                        cb(result);
-                    });
+                        closestUp, closestDown, checkBetween, function (result) {
+                            //craw the result
+                            crawlStaredRepos(result);
+                            //deliver the result
+                            cb(result);
+                        });
                 } else {
                     switch (result) {
                         case 0:
                             //恰当，可以不用做两分法测试了,应该直接使用该值抓取数据
                             //由于是首次抓取所以情况特殊，searchTerm应使用特别语句生成
-                            stadardPageCrawler(currentGuess);
+                            crawlStaredRepos(currentGuess);
                             cb(currentGuess);
                             break;
                         case 1:
@@ -71,7 +137,7 @@ var dosomething = function(ruler, cb) {
                             //zai shang mian de ci shu
                             upTimes++;
                             closestUp = currentGuess;
-                            firstOperation();
+                            nextStep();
                             break;
                         case -1:
                             //返回结果数量太少， 增加猜想值
@@ -87,82 +153,16 @@ var dosomething = function(ruler, cb) {
                             lastOperation.operation = 'up';
                             downTimes++;
                             closestDown = currentGuess;
-                            firstOperation();
+                            nextStep();
                             break;
                     }
                 }
             });
-        }
-
-    } else {
-        //first Operation has been done
-        //use the latest info in the array
-        var gap = (rulerLength > 1) ? (lastRuler - ruler[rulerLength - 2]) : FIRSTGAP;
-        //guess the current gap using the latest gap
-        var guess = lastRuler - gap / 0.9;
-        github.search.staredRepos.checkLength({
-            type: '..',
-            args: [guess, lastRuler]
-        }, function(err, result) {
-            if (err) throw err;
-            if (downTimes * upTimes > 0) {
-                //如果预测值的上下数据都有了
-                //现在才是两分法啊！！！尼玛，算法太复杂了
-                bisection(
-                    closestUp, closestDown, checkBetween, function(result) {
-                    //craw the result
-                    stadardPageCrawler(result);
-                    //diliver the result
-                    cb(result);
-                });
-            } else {
-                switch (result) {
-                    case 0:
-                        //恰当，可以不用做两分法测试了,应该直接使用该值抓取数据
-                        //由于是首次抓取所以情况特殊，searchTerm应使用特别语句生成
-                        stadardPageCrawler(currentGuess);
-                        cb(currentGuess);
-                        break;
-                    case 1:
-                        //返回结果数量太多， 减少猜想值
-                        //继续
-                        if (lastOperation.operation === 'down') {
-                            lastOperation.optimes++;
-                            currentGuess -= INCREMENT * lastOperation.optimes;
-                        } else {
-                            //rest optimes
-                            lastOperation.optimes = 0;
-                            currentGuess -= INCREMENT;
-                        }
-                        lastOperation.operation = 'down';
-                        //zai shang mian de ci shu
-                        upTimes++;
-                        closestUp = currentGuess;
-                        firstOperation();
-                        break;
-                    case -1:
-                        //返回结果数量太少， 增加猜想值
-                        //go on
-                        if (lastOperation.operation === 'up') {
-                            lastOperation.optimes++;
-                            currentGuess += INCREMENT * lastOperation.optimes;
-                        } else {
-                            //rest optimes
-                            lastOperation.optimes = 0;
-                            currentGuess += INCREMENT;
-                        }
-                        lastOperation.operation = 'up';
-                        downTimes++;
-                        closestDown = currentGuess;
-                        firstOperation();
-                        break;
-                }
-            }
-        })
+        })();
     }
-};
+}
 
-var checkBiggerThan = function(m, callback) {
+var checkBiggerThan = function (m, callback) {
     //根据middle值，生成request参数
     var params = {
         urlAppend: {
@@ -170,12 +170,12 @@ var checkBiggerThan = function(m, callback) {
             args: [m]
         }
     };
-    github.search.staredRepos.checkLength(params, function(err, result) {
+    github.search.staredRepos.checkLength(params, function (err, result) {
         callback(err, result);
     });
 };
 
-var checkLesserThan = function(m, callback) {
+var checkLesserThan = function (m, callback) {
     //根据middle值，生成request参数
     var params = {
         urlAppend: {
@@ -183,12 +183,12 @@ var checkLesserThan = function(m, callback) {
             args: [m]
         }
     };
-    github.search.staredRepos.checkLength(params, function(err, result) {
+    github.search.staredRepos.checkLength(params, function (err, result) {
         callback(err, result);
     });
 };
 
-var checkEqualsTo = function(m, callback) {
+var checkEqualsTo = function (m, callback) {
     //根据middle值，生成request参数
     var params = {
         urlAppend: {
@@ -196,45 +196,51 @@ var checkEqualsTo = function(m, callback) {
             args: [m]
         }
     };
-    github.search.staredRepos.checkLength(params, function(err, result) {
+    github.search.staredRepos.checkLength(params, function (err, result) {
         callback(err, result);
     });
 };
 
-var checkBetween = function(m, callback) {
+var checkBetween = function (m, callback) {
     var params = {
         urlAppend: {
             type: '..',
             args: [m, blablabla]
         }
     };
-    github.search.staredRepos, checkLength(params, function(err, re) {
+    github.search.staredRepos.checkLength(params, function (err, re) {
         callback(err, result);
     });
+};
+
+var crawlStaredRepos = function () {
+
 }
 
-    function getStaredRepositories() {
+function getStaredRepositories() {
 
-        var ruler = {
-            ruler: [],
-            pointer: 0
-        };
+    var ruler = {
+        ruler: [],
+        pointer: 0
+    };
 
-        var loop = new Infiniteloop();
+    var loop = new Infiniteloop();
 
-        function grab() {
-            dosomething(ruler, function(x) {
-                //忘了status是用来干嘛的了。。汗
-                if (status === true) {
-                    ruler.data.push(x);
-                    ruler.pointer++;
-                } else {
-                    loop.stop();
-                }
+    function grab() {
+        dosomething(ruler, function (x) {
+            //忘了status是用来干嘛的了。。汗
+            if (status === true) {
+                ruler.data.push(x);
+                ruler.pointer++;
+            } else {
+                loop.stop();
+            }
 
-            });
-        }
-
-        loop.add(grab).run();
-
+        });
     }
+
+    loop.add(grab).run();
+
+}
+
+
